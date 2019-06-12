26d3d3c071c709b3d7aeb67f634aaebc7c026d38
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2019-06-12 21:07:51 +0800

    Add files via upload

--- Script-014b-StemLollipops-ordered.py
+++ Script-014b-StemLollipops-ordered.py
@@ -1,28 +1,23 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[9]:
-
-
-# Libraries
+#
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import os
- 
+#
 # Loading data
 os.chdir('/Users/pauline/Documents/Python')
 df = pd.read_csv("Tab-Morph.csv")
-df = pd.DataFrame({'group':df.profile, 'values':df.tg_angle}) # take only two values: bath. profiles and angles
-#df
-
-# Reorder it following the values:
+df = pd.DataFrame({'group':df.profile, 'values':df.tg_angle}) # take 2 values: profiles and angles
+#
+# Reorder it by the values
 ordered_df = df.sort_values(by='values') # sort df by values
- 
-# Making the plot
-markerline, stemlines, baseline = plt.stem(x, ordered_df['values'], markerfmt="o", basefmt='-')
-
-# Setting properties
+#
+# Plotting
+markerline, stemlines, baseline = plt.stem(ordered_df['values'], markerfmt="o", basefmt='-')
+#
+# Design
 plt.setp(markerline, mfc='salmon', markersize=13, alpha=0.7, markeredgewidth=.2) 
 plt.setp(stemlines, linewidth=.5, color='dimgray') 
 plt.setp(baseline, color='purple', linewidth=1)
@@ -30,4 +25,3 @@ plt.suptitle('Geomorphologycal analysis of the Mariana Trench: \nSorted plot by 
 plt.xlabel('Bathymetric profiles', fontsize=10, fontfamily='sans-serif')
 plt.ylabel('tg$^\circ$ (A/H)', fontsize=10, fontfamily='sans-serif')
 plt.show()
-
