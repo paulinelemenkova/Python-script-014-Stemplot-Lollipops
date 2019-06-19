d7a4780669a3f5b70c206afeff1da025aad8100b
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2019-06-19 22:23:42 +0800

    Add files via upload

--- Script-014-Stems.py
+++ Script-014-Stems.py
@@ -1,50 +1,54 @@
 #!/usr/bin/env python
 # coding: utf-8
-#
-import matplotlib.pyplot as plt
+import os
 import numpy as np
 import pandas as pd
-import os
+import matplotlib.pyplot as plt
 import pylab as plt
-#
+
 os.chdir('/Users/pauline/Documents/Python')
 df = pd.read_csv("Tab-Morph.csv")
-#
+
 # define figure
-fig = plt.figure()
-st = fig.suptitle('Geomorphologycal analysis of the Mariana Trench: 25 bathymetric profiles \nunsorted (A) and sorted (B) plots by steepness angles', 
-             fontsize=12)
+fig = plt.figure(figsize=(5.0, 5.0), dpi=300)
+fig.suptitle('Geomorphologycal analysis of the Mariana Trench: \n25 cross-section profiles unsorted (A) and sorted (B) \n by steepness gradient',
+             fontsize=9, fontfamily='serif', fontweight='normal', x=0.5, y=0.97
+             )
 ax1 = fig.add_subplot(211)
 ax2 = fig.add_subplot(212)
-#
-# define subplot 1
+
+# subplot 1
 plt.subplot(2, 1, 1)
-x=df.profile
-values=df.tg_angle
-markerline, stemlines, baseline = plt.stem(x, values, markerfmt="o", basefmt='-')
-plt.setp(markerline, mfc='salmon', markersize=13, alpha=0.7, markeredgewidth=.2) 
-plt.setp(stemlines, linewidth=.5, color='dimgray') 
-plt.setp(baseline, color='purple', linewidth=1)
-plt.xlabel('Bathymetric profiles', fontsize=10, fontfamily='sans-serif')
-plt.ylabel('tg$^\circ$ (A/H)', fontsize=10, fontfamily='sans-serif')
-#
-# define subplot 2
-plt.subplot(2, 1, 2)
-os.chdir('/Users/pauline/Documents/Python')
-df = pd.read_csv("Tab-Morph.csv")
-df = pd.DataFrame({'group':df.profile, 'values':df.tg_angle}) # take 2 values: profiles and angles
-ordered_df = df.sort_values(by='values') # sort df by values
-markerline, stemlines, baseline = plt.stem(x, ordered_df['values'], markerfmt="o", basefmt='-')
-#
-# plotting
-plt.setp(markerline, mfc='salmon', markersize=13, alpha=0.7, markeredgewidth=.2) 
-plt.xticks( x, ordered_df['group'])
-plt.setp(stemlines, linewidth=.5, color='dimgray') 
+x = df.profile
+values = df.tg_angle
+markerline, stemlines, baseline = plt.stem(x, values, markerfmt="o",
+                                           basefmt='-', use_line_collection=True)
+plt.setp(markerline, mfc='salmon', markersize=13, alpha=0.7, markeredgewidth=.2)
+plt.setp(stemlines, linewidth=.5, color='dimgray')
 plt.setp(baseline, color='purple', linewidth=1)
 plt.xlabel('Bathymetric profiles', fontsize=10, fontfamily='sans-serif')
 plt.ylabel('tg$^\circ$ (A/H)', fontsize=10, fontfamily='sans-serif')
 ax1.annotate('(A)', xy=(1.02, .90), xycoords="axes fraction")
+
+# subplot 2
+plt.subplot(2, 1, 2)
+df = pd.DataFrame({'group':df.profile, 'values':df.tg_angle})
+ordered_df = df.sort_values(by='values')
+markerline, stemlines, baseline = plt.stem(x, ordered_df['values'],
+                                           markerfmt="o", basefmt='-', use_line_collection=True)
+plt.setp(markerline, mfc='salmon', markersize=13, alpha=0.7, markeredgewidth=.2)
+plt.xticks(x, ordered_df['group'], fontsize=8)
+plt.setp(stemlines, linewidth=.5, color='dimgray')
+plt.setp(baseline, color='purple', linewidth=1)
+plt.xlabel('Bathymetric profiles', fontsize=10, fontfamily='sans-serif')
+plt.ylabel('tg$^\circ$ (A/H)', fontsize=10, fontfamily='sans-serif')
 ax2.annotate('(B)', xy=(1.02, .90), xycoords="axes fraction")
-# 0,0 is lower left of axes and 1,1 is upper right
-plt.subplots_adjust(hspace=.2)
+
+# visualizing
+plt.tight_layout()
+plt.subplots_adjust(top=0.85, bottom=0.08,
+                    left=0.10, right=0.95,
+                    hspace=0.25, wspace=0.35
+                    )
+plt.savefig('plot_Stems.png', dpi=300)
 plt.show()
